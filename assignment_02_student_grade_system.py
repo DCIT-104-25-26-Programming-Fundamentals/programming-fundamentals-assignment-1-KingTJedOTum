b4e7dcd249c6afeccf2c0b20aa3592b8417e5cd4
KingTJedOTum <ktjotum@st.ug.edu.gh>
2026-07-27 17:34:35 +0000

    Adding grade Calculation logic

--- assignment_02_student_grade_system.py
+++ assignment_02_student_grade_system.py
@@ -48,4 +48,15 @@
  
 
 def get_grade(score):
-    pass
+    if score >= 80:
+        return "A"
+    elif score >= 70:
+        return "B"
+    elif score >= 60:
+        return "C"
+    elif score >= 50:
+        return "D"
+    else:
+        return "F"
+    
+    
